03d784dd2e7f87fd5a5b5bc75de884b59998bd26
yujinlee1234 <godgod5328@gmail.com>
2016-12-26 16:14:29 +0900

    Test 완료

---
+++ src/main/java/kr/or/dgit/bigdata/school/TestMain.java
@@ -0,0 +1,11 @@
+package kr.or.dgit.bigdata.school;
+
+import javax.swing.JFrame;
+
+public class TestMain {
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setTitle("테스트");
+		jf.setVisible(true);
+	}
+}
